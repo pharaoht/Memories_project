97c0a70c63e782dd6d29e40d23532abc51e8081b
pharaoh <pharaohmanson@gmail.com>
2021-01-08 18:39:38 -0500

    Added more client side redux code to handle action requests

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyles from './styles';
 
 const Form = () => {
     const classes = useStyles();
+    const handleSubmit = () => {
+        
+    } 
+
     return(
-        <h1>Form</h1>
+        <Paper className={ classes.paper }>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+
+            </form>
+        </Paper>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes)
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes)
+
 
 const CONNECTION_URL = 'mongodb+srv://pharaohmanson:pharaohmanson123@cluster0.2zblb.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
